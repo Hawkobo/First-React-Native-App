50f3d737210689beadbdbfec053ee89c10a133ab
Jacob Young <jwyoung07@gmail.com>
2020-07-16 18:34:16 -0700

    first upload

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Image, Alert, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>Hello Jojo!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Press me!"
+        color="#4287f5"
+        accessibilityLabel="Don't press me"
+        />
+      <Image
+        source={{uri: 'https://cdn.discordapp.com/attachments/417821911301488651/733124698434568282/BF_Flame_and_Letter_Logo4x.png',}}
+        resizeMode="center"
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  titleText: {
+    fontFamily: "Times New Roman"
+  },
   container: {
     flex: 1,
+    padding: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
